aad20615fea9e273aefd96bea6fa1174e50ae265
aarif-s <108191982+aarif-s@users.noreply.github.com>
2024-03-17 20:52:26 +0000

    comparison

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,21 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null > 0); // false      // null shows 0 or some time NAN so diffrent ans
+console.log(null == 0); // false 
+console.log(null >= 0); // true 
+
+console.log(undefined == 0); // false 
+console.log(undefined > 0);  // false 
+console.log(undefined < 0);  // false 
+
+// ===     // Strict comaprision we can't comapre String to number 
+
+console.log("2" === 2);   // false 
