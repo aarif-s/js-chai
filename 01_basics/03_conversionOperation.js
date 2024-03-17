e0a0415997cd9612a4b70787f3ec393068c4cc43
aarif-s <108191982+aarif-s@users.noreply.github.com>
2024-03-17 20:26:39 +0000

    conversion and operators

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -24,3 +24,45 @@ let someNumbeer = 33;
 
 let stringNumber = String (someNumbeer)
 console.log(typeof stringNumber);
+
+
+// ************** Operations ******************************
+
+let value =3
+let negvalue = -value
+console.log(negvalue);
+
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**2);
+console.log(2/2);
+console.log(2%2);
+
+let str1 = "hello"
+let str2 = " hitesh"
+
+let str3 = str1 +atr2
+console.log(str3);
+
+console.log("1" + 2); //12
+console.log(1 +"2"); // 12
+console.log("1" +2 +2); // 122
+console.log(1 + 2 + "2"); // 32
+
+console.log(true); // true
+console.log(+true); // 1  increment 
+console.log(+""); // 0
+
+let num1 , num2 , num3 
+
+um1+ num2 + num3 = 2+2
+
+let gameOunter = 100;
+
+gameCounter++;  // prefix and poxfix increment should learn 
+console.log(gameCounter);
+
+//  link to study 
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
