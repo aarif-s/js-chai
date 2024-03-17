5a79642dc6035b6d86cd84af8a3eb7fc041337c0
aarif-s <108191982+aarif-s@users.noreply.github.com>
2024-03-17 19:44:14 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,31 @@
+"use strict"; // treat all js as never version
+
+// alert(3+3); we are using node js not browser
+
+console.log(3+
+    3);
+// code readibility
+
+//  MDN documentation
+// original documentation tc39.es // ECMA Script
+
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+
+// (primitive data types)
+
+// number => range 2 to powerr 53
+// bigint 
+// string => ""
+// boolean => true/false 
+// null => standalone values
+// undefined => value assign nhi hue h
+// symbol => unique 
+
+// Object
+console.log(typeof name);  // String
+console.log(typeof age); // number 
+console.log(typeof null);  // object 
+console.log(typeof undefined); // undefined   
+
