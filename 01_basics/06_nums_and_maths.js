6dcf21bf74033dcd2db7e4d233abf6e912702f9e
aarif-s <108191982+aarif-s@users.noreply.github.com>
2024-03-19 14:46:22 +0000

    Nums and maths

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,24 @@
+const score = 400;
+console.log();
+
+const balance = new Number(100)
+console.log(balance);
+
+console.log(balance.toString().length); 
+console.log(balance.toFixed(3));   // specially for ecomerce website 
+
+const othernumber = 123.8976
+ console.log(othernumber.toPrecision(3)); // decimal ke phle aesy the but uske baad dhyann se 
+
+const hundred = 1000000
+console.log(hundred.toLocaleString(`en-IN`));
+
+// ****************Maths*************************************************
+
+console.log(Math);
+
+
+
+
+
+
