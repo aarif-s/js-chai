160199300f7d4f4c8f8b4de88dda5dc42a5cd168
aarif-s <108191982+aarif-s@users.noreply.github.com>
2024-03-20 17:53:45 +0000

    nums and maths

--- 01_basics/06_nums_and_maths.js
+++ 01_basics/06_nums_and_maths.js
@@ -1,21 +1,37 @@
-const score = 400;
-console.log();
+const score = 400
+// console.log(score);
 
 const balance = new Number(100)
-console.log(balance);
+// console.log(balance);
 
-console.log(balance.toString().length); 
-console.log(balance.toFixed(3));   // specially for ecomerce website 
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(1));
 
-const othernumber = 123.8976
- console.log(othernumber.toPrecision(3)); // decimal ke phle aesy the but uske baad dhyann se 
+const otherNumber = 123.8966
 
-const hundred = 1000000
-console.log(hundred.toLocaleString(`en-IN`));
+// console.log(otherNumber.toPrecision(4));
 
-// ****************Maths*************************************************
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));
 
-console.log(Math);
+// +++++++++++++ Maths +++++++++++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.9));
+// console.log(Math.min(4, 3, 6, 8));
+// console.log(Math.max(4, 3, 6, 8));
+
+console.log(Math.random());
+console.log((Math.random()*10) + 1);
+console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
 
 
 
