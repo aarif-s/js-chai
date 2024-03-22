960fb738a20dc3f34da9994ec84c422ca2da588f
aarif-s <108191982+aarif-s@users.noreply.github.com>
2024-03-22 10:04:31 +0000

    objects

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -23,7 +23,7 @@ const JsUser = {
 // console.log(JsUser[mySym])
 
 JsUser.email = "hitesh@chatgpt.com"
-// Object.freeze(JsUser)
+// Object.freeze(JsUser)              // for no further change 
 JsUser.email = "hitesh@microsoft.com"
 // console.log(JsUser);
 
@@ -31,7 +31,7 @@ JsUser.greeting = function(){
     console.log("Hello JS user");
 }
 JsUser.greetingTwo = function(){
-    console.log(`Hello JS user, ${this.name}`);
+    console.log(`Hello JS user, ${this.name}`);     
 }
 
 console.log(JsUser.greeting());
